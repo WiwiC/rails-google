fbd10d09af71a81c428131798500ca732c9cd665
William <william.chalons@gmail.com>
2017-08-09 16:44:48 +0200

    Add javascript

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,28 @@
+$(document).ready(function() {
+
+var afficher = document.querySelector('button');
+afficher.onclick = function () {
+    document.getElementById("id_hidden").className = "";
+    afficher.className = "hidden";
+}
 
 
+var search = document.forms["formulaire"].search_bar;
+var click = document.forms["formulaire"].boutton_click;
+
+click.onclick = function () {
+    var value = search.value.trim();
+    if (value != "" ) {
+        var mot_a_rechercher = value.replace(/( )+/g, "+");
+        document.location.href="https://www.google.fr/#q=" + mot_a_rechercher;
+    }
+    else {
+    }
+}
+
+
+function pop_up() {
+
+}
+setTimeout(pop_up, 5000);
+});
